fa87da3552514d96c5017342438ab17911c11b70
Felix <fexlixhoang7@gmail.com>
2024-11-05 19:53:51 -0800

    Rotates now but cannot drag

--- A Slice of Lunch/Assets/Scripts/CharacterControls/PlayerRotate.cs
+++ A Slice of Lunch/Assets/Scripts/CharacterControls/PlayerRotate.cs
@@ -1,20 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerRotate : MonoBehaviour
 {
-    //vectors meant to instantiate position of mouse/cursor
-    Vector3 prevPosition = Vector3.zero;
-    Vector3 positionDelta = Vector3.zero;
+    private Camera myCam;
+    private Vector3 screenPosition;
+    private float angleOffset;
+    private Collider2D col;
 
-    public RigidBody2d rb;
-    // Update is called once per frame
-    void Update()
+    private void Start() {
+        myCam = Camera.main;
+        col = GetComponent<Collider2D>();
+    }
+    
+    private void Update()
     {
-        if(input.GetMouseButton(0))
+        RotateFood();
+    }
+
+    private void RotateFood()
+    {
+        Vector3 mousePos = myCam.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
         {
-            positionDelta = Input.mousePosition - prevPosition;
+            if (col == Physics2D.OverlapPoint(mousePos))
+            {
+                screenPosition = myCam.WorldToScreenPoint(transform.position);
+                Vector3 vec3 = Input.mousePosition - screenPosition;
+                angleOffset = (Mathf.Atan2(transform.right.y, transform.right.x) - Mathf.Atan2(vec3.y, vec3.x)) * Mathf.Rad2Deg;
+            }
+        }
+        if (Input.GetMouseButton(0))
+        {
+            if (col == Physics2D.OverlapPoint(mousePos))
+            {
+                Vector3 vec3 = Input.mousePosition - screenPosition;
+                float angle = Mathf.Atan2(vec3.y, vec3.x) * Mathf.Rad2Deg;
+                transform.eulerAngles = new Vector3(0, 0, angle + angleOffset);
+            }
         }
     }
 }

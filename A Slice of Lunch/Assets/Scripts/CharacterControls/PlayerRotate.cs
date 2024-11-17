ba675ad97a532c4a170ef0d562d9d5cf686e5fea
Felix <fexlixhoang7@gmail.com>
2024-11-17 11:54:56 -0800

    rotation finished (needs tweaks)

--- A Slice of Lunch/Assets/Scripts/CharacterControls/PlayerRotate.cs
+++ A Slice of Lunch/Assets/Scripts/CharacterControls/PlayerRotate.cs
@@ -7,48 +7,38 @@ public class PlayerRotate : MonoBehaviour
 {
     private Camera myCam;
     private Vector3 screenPosition;
-    private float angleOffset;
-    private Collider2D col;
+    //private float angleOffset;
+    private float angle;
 
     private void Start() {
         myCam = Camera.main;
-        col = GetComponent<Collider2D>();
     }
     
     private void Update()
     {
-
-    
         Ray clickRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit2D hit2D =  Physics2D.GetRayIntersection(clickRay);
         if (hit2D.collider != null && hit2D.collider.CompareTag("Food"))
         {
-            RotateFood();
-        }
-        
+        RotateFood(hit2D.collider);
+            
+
+        }   
     }
 
-    private void RotateFood()
+    private void RotateFood(Collider2D col)
     {
         Vector3 mousePos = myCam.ScreenToWorldPoint(Input.mousePosition);
-        if (Input.GetMouseButtonDown(0))
-        {
             if (col == Physics2D.OverlapPoint(mousePos))
             {
                 screenPosition = myCam.WorldToScreenPoint(transform.position);
                 Vector3 vec3 = Input.mousePosition - screenPosition;
-                angleOffset = (Mathf.Atan2(transform.right.y, transform.right.x) - Mathf.Atan2(vec3.y, vec3.x)) * Mathf.Rad2Deg;
+                //angleOffset = (Mathf.Atan2(transform.right.y, transform.right.x) - Mathf.Atan2(vec3.y, vec3.x)) * Mathf.Rad2Deg;
+                angle = Mathf.Atan2(vec3.y, vec3.x) * Mathf.Rad2Deg;
+                col.transform.eulerAngles = new Vector3(0, 0, angle);
+                Debug.Log("I am rotating!");
+
             }
-        }
-        if (Input.GetMouseButton(0))
-        {
-            if (col == Physics2D.OverlapPoint(mousePos))
-            {
-                Vector3 vec3 = Input.mousePosition - screenPosition;
-                float angle = Mathf.Atan2(vec3.y, vec3.x) * Mathf.Rad2Deg;
-                transform.eulerAngles = new Vector3(0, 0, angle + angleOffset);
-            }
-        }
 
     }
 }

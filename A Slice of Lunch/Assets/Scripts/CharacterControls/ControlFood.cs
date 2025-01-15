e8e495fe997b5fd266d0d00dacfaa06fad99a5a2
Jhoshua Ampo <ampojhoshua@gmail.com>
2025-01-15 12:22:30 -0800

    implement dragged food to be over other foods

--- A Slice of Lunch/Assets/Scripts/CharacterControls/ControlFood.cs
+++ A Slice of Lunch/Assets/Scripts/CharacterControls/ControlFood.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class ControlFood : MonoBehaviour
 {
     bool dragging = false;
     PlayerControls playerControls;
+    SortingGroup sortingGroup;
+    int initialSortingOrder;
     
     private void Awake() {
         playerControls = GameObject.FindWithTag("Player").GetComponent<PlayerControls>();
+        sortingGroup = GetComponentInParent<SortingGroup>();
+        initialSortingOrder = sortingGroup.sortingOrder;
     }
 
     private void OnMouseDown() {
@@ -21,6 +26,10 @@ public class ControlFood : MonoBehaviour
             Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             pos.z = 0f;
             transform.parent.position = pos;
+            sortingGroup.sortingOrder = 100;
+        }
+        else {
+            sortingGroup.sortingOrder = initialSortingOrder;
         }
     }
 

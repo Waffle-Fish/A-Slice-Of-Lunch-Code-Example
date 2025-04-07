73a266f15960cfad013cad5c78276fa9a219f6f2
Jhoshua Ampo <ampojhoshua@gmail.com>
2025-04-07 09:46:28 -0700

    update select menu

--- A Slice of Lunch/Assets/Scripts/Utility/CustomShapedButton.cs
+++ A Slice of Lunch/Assets/Scripts/Utility/CustomShapedButton.cs
@@ -3,6 +3,9 @@ using UnityEngine.UI;
 
 public class CustomShapedButton : MonoBehaviour
 {
+    [Tooltip("Minimum alpha a pixel must have for the event to considered a hit on the Image")]
+    [Range(0,1f)]
+    [SerializeField] float alphaHitTestMinimumThreshold = 0.5f;
     private Image buttonImg;
 
     void Awake() {
@@ -11,6 +14,6 @@ public class CustomShapedButton : MonoBehaviour
 
     void Start()
     {
-        buttonImg.alphaHitTestMinimumThreshold = 0.5f;
+        buttonImg.alphaHitTestMinimumThreshold = alphaHitTestMinimumThreshold;
     }
 }

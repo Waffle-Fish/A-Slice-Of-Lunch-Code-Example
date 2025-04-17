b6804bf439761e77015c527e2b043900e33556b0
Jhoshua Ampo <ampojhoshua@gmail.com>
2025-04-17 14:25:09 -0700

    update menu

---
+++ A Slice of Lunch/Assets/Scripts/Utility/ButtonSFX.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(AudioSource))]
+public class ButtonSFX : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler
+{
+    [SerializeField] List<AudioClip> clickSFXS;
+    [SerializeField] List<AudioClip> hoverSFXS;
+
+    AudioSource audioSource;
+    private void Awake() {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.loop = false;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (clickSFXS.Count == 0) return;
+        audioSource.PlayOneShot(clickSFXS[Random.Range(0, clickSFXS.Count)]);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (hoverSFXS.Count == 0) return;
+        audioSource.PlayOneShot(hoverSFXS[Random.Range(0, hoverSFXS.Count)]);
+    }
+}

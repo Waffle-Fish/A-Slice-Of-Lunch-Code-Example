f30bdb9cffa917cec076b1e5cd1870bb5ac82d3f
Jhoshua Ampo <ampojhoshua@gmail.com>
2025-04-17 21:13:54 -0700

    big update

--- A Slice of Lunch/Assets/Scripts/Menu/BoxMenuControls.cs
+++ A Slice of Lunch/Assets/Scripts/Menu/BoxMenuControls.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,41 +9,57 @@ public class BoxMenuControls : MonoBehaviour
     public float shiftValue;
     [Tooltip("How long in secs shifting takes")]
     [SerializeField] float shiftDuration = 1f;
+    [SerializeField] Animator leftButton;
+    [SerializeField] Animator rightButton;
 
     RectTransform boxHolder;
     int numBoxes = 0;
     float maxXVal = 0;
+    int currentBox = 0;
     public bool IsActive {get; private set;} = false;
+
+    List<Animator> animators = new();
 
     void Awake()
     {
         boxHolder = GetComponent<RectTransform>();
+        GetComponentsInChildren<Animator>(animators);
     }
     void Start()
     {
-        numBoxes = GetComponentsInChildren<Button>().GetLength(0);
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.activeSelf) numBoxes++;
+        }
         maxXVal = -(numBoxes - 1) * shiftValue;
     }
 
     public void GoLeft() {
         if(IsActive) return;
         IsActive = true;
+        CloseAllNotePads();
         StartCoroutine(ShiftBox(true));
     }
 
     public void GoRight() {
         if(IsActive) return;
         IsActive = true;
+        CloseAllNotePads();
         StartCoroutine(ShiftBox(false));
+        
     }
 
     // true = left; false = right
-    private IEnumerator ShiftBox(bool dir) {
-        float xGoal = boxHolder.anchoredPosition.x + (dir ? shiftValue : -shiftValue);
+    private IEnumerator ShiftBox(bool goLeft) {
+        
+        if (currentBox != 0) leftButton.SetTrigger("FadeOut");
+        if (currentBox != numBoxes -1) rightButton.SetTrigger("FadeOut");
+
+        float xGoal = boxHolder.anchoredPosition.x + (goLeft ? shiftValue : -shiftValue);
         xGoal = Mathf.Clamp(xGoal, maxXVal, 0f);
-        float rate = ((dir ? shiftValue : -shiftValue) / shiftDuration) * Time.deltaTime;
+        float rate = ((goLeft ? shiftValue : -shiftValue) / shiftDuration) * Time.deltaTime;
         Vector2 newPos = boxHolder.anchoredPosition;
-        if (dir) {
+        if (goLeft) {
             while(boxHolder.anchoredPosition.x < xGoal)
             {
                 newPos.x += rate;
@@ -60,6 +78,24 @@ public class BoxMenuControls : MonoBehaviour
             newPos.x = xGoal;
             boxHolder.anchoredPosition = newPos;
         }
+        currentBox += (goLeft) ? -1 : 1;
+        currentBox = Mathf.Clamp(currentBox, 0, numBoxes-1);
+
+        if (!leftButton.gameObject.activeSelf) {leftButton.gameObject.SetActive(true);}
+        if (!rightButton.gameObject.activeSelf) {rightButton.gameObject.SetActive(true);}
+        if (currentBox != 0) leftButton.SetTrigger("FadeIn");
+        if (currentBox != numBoxes -1) rightButton.SetTrigger("FadeIn");
+        
+
         IsActive = false;
+    }
+
+    private void CloseAllNotePads() {
+        foreach (var anim in animators)
+        {
+            anim.ResetTrigger("Open");
+            
+            anim.SetTrigger("Close");
+        }
     }
 }

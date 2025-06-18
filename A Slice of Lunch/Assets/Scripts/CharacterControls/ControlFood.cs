baa0775b2cd65eb7730a54f0f6a4398ba5657056
Jhoshua Ampo <ampojhoshua@gmail.com>
2025-06-17 20:27:56 -0700

    Implement ControlFood with event system functions

--- A Slice of Lunch/Assets/Scripts/CharacterControls/ControlFood.cs
+++ A Slice of Lunch/Assets/Scripts/CharacterControls/ControlFood.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.Rendering;
 
-public class ControlFood : MonoBehaviour /*, IPointerDownHandler, IPointerUpHandler*/
+public class ControlFood : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     public string TextureSFX = "";
     public string TableTextureSFX = "";
@@ -17,58 +18,68 @@ public class ControlFood : MonoBehaviour /*, IPointerDownHandler, IPointerUpHand
         originalPosition = parentTransform.position;
     }
 
-    // private void OnTriggerEnter2D(Collider2D other) {
-    //     if (other.CompareTag("Food")) {
-    //         onFood = true;
-    //     }
-    // }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Food")) {
+            onFood = true;
+        }
+    }
 
-    // private void OnTriggerStay2D(Collider2D other) {
-    //     if (other.CompareTag("Food")) {
-    //         onFood = true;
-    //     }
-    // }
+    private void OnTriggerStay2D(Collider2D other) {
+        if (other.CompareTag("Food")) {
+            onFood = true;
+        }
+    }
 
-    // private void OnTriggerExit2D(Collider2D other) {
-    //     if (other.CompareTag("Food")) {
-    //         onFood = false;
-    //     }
-    // }
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.CompareTag("Food")) {
+            onFood = false;
+        }
+    }
 
-    // private IEnumerator HandleFoodCollision() {
-    //     float numIterations = 50;
-    //     float totalTime = 0.01f;
-    //     float timePerIteration = totalTime / numIterations;
-    //     Vector3 startPos = parentTransform.position;
-    //     Vector3 endPos = originalPosition;
-    //     for (float i = 0; i < totalTime; i+= timePerIteration)
-    //     {
-    //         parentTransform.position = Vector3.Lerp(startPos, endPos, i / totalTime);
-    //         yield return new WaitForSeconds(timePerIteration);
-    //     }
-    // }
+    private IEnumerator HandleFoodCollision() {
+        float numIterations = 50;
+        float totalTime = 0.01f;
+        float timePerIteration = totalTime / numIterations;
+        Vector3 startPos = parentTransform.position;
+        Vector3 endPos = originalPosition;
+        for (float i = 0; i < totalTime; i+= timePerIteration)
+        {
+            parentTransform.position = Vector3.Lerp(startPos, endPos, i / totalTime);
+            yield return new WaitForSeconds(timePerIteration);
+        }
+    }
 
-    // public void PlayGrabSFX()
-    // {
-    //     if (textureSFX == "FoodCrunch")
-    //     {
-    //         AudioManager.Instance.PlaySFX("FoodCrunch");
-    //     }
-    //     else if (textureSFX == "FoodSquish")
-    //     {
-    //         AudioManager.Instance.PlaySFX("FoodSquish");
-    //     }
-    // }
+    public void PlayGrabSFX()
+    {
+        if (TextureSFX == "FoodCrunch")
+        {
+            AudioManager.Instance.PlaySFX("FoodCrunch");
+        }
+        else if (TextureSFX == "FoodSquish")
+        {
+            AudioManager.Instance.PlaySFX("FoodSquish");
+        }
+    }
 
-    // public void PlayDropSFX()
-    // {
-    //     if (tableTextureSFX == "Wood")
-    //     {
-    //         AudioManager.Instance.PlaySFX("PlaceOnWood");
-    //     }
-    //     else
-    //     {
-    //         AudioManager.Instance.PlaySFX("PlaceOnWood");
-    //     }
-    // }
+    public void PlayDropSFX()
+    {
+        if (TableTextureSFX == "Wood")
+        {
+            AudioManager.Instance.PlaySFX("PlaceOnWood");
+        }
+        else
+        {
+            AudioManager.Instance.PlaySFX("PlaceOnWood");
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Pointer down on " + parentTransform.name);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("Pointer up on " + parentTransform.name);
+    }
 }

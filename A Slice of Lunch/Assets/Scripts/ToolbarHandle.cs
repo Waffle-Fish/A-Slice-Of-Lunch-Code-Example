f85847817f9e337718bd46b35a60c3ecb03824a5
Jhoshua Ampo <ampojhoshua@gmail.com>
2025-09-22 18:21:14 -0700

    implement toolbar
    Open and close animation

---
+++ A Slice of Lunch/Assets/Scripts/ToolbarHandle.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ToolbarHandle : MonoBehaviour, IPointerClickHandler
+{
+    Animator animator;
+    bool isOpen = true;
+
+    private void Awake()
+    {
+        animator = transform.parent.GetComponent<Animator>();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        isOpen = !isOpen;
+        animator.SetBool("Open", isOpen);
+    }
+}
